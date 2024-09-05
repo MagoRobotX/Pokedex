cf3c9e241d070686f0d87532ab3c5414bc01f6e6
MagoRobotX <lopezrobotf@gmail.com>
2024-09-05 10:16:33 -0600

    actualizados

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
+
 }
 
 android {
@@ -33,37 +35,43 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    viewBinding{
+    viewBinding {
         enable = true
     }
-    buildFeatures{
+    buildFeatures {
         viewBinding = true
+
     }
+
 }
 
+    dependencies {
+
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.appcompat)
+        implementation(libs.material)
+        implementation(libs.androidx.activity)
+        implementation(libs.androidx.constraintlayout)
+        implementation(libs.androidx.tracing.perfetto.handshake)
+        testImplementation(libs.junit)
+        // Retrofit
+        implementation("com.squareup.retrofit2:retrofit:2.9.0")
+        // Retrofit with Scalar Converter
+        implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+        implementation("com.squareup.picasso:picasso:2.71828")
+        implementation("com.squareup.picasso:picasso:2.8")
+
+        //picasso
+        implementation("com.squareup.picasso:picasso:2.8")
+        implementation("com.github.bumptech.glide:glide:4.12.0")
+        annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+        //sql
+        implementation("androidx.room:room-runtime:2.5.1")
+        implementation("androidx.room:room-ktx:2.5.1")
+        implementation("androidx.core:core-ktx:1.10.1")
 
 
-dependencies {
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.tracing.perfetto.handshake)
-    testImplementation(libs.junit)
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    // Retrofit with Scalar Converter
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.picasso:picasso:2.71828")
-    implementation ("com.squareup.picasso:picasso:2.8")
-
-    //picasso
-    implementation ("com.squareup.picasso:picasso:2.8")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
-
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-}
+    }
